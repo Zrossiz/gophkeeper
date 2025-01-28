ed7c5b687aa577a046511c2cb9f623073cbaacf8
Zrossiz <soon.pluxury@mail.ru>
2025-01-28 22:09:50 +0300

    feat: init app

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/Zrossiz/gophkeeper/internal/config"
 	"github.com/Zrossiz/gophkeeper/internal/service"
@@ -9,6 +10,7 @@ import (
 	"github.com/Zrossiz/gophkeeper/internal/transport/http/handler"
 	"github.com/Zrossiz/gophkeeper/internal/transport/http/router"
 	"github.com/Zrossiz/gophkeeper/pkg/logger"
+	"go.uber.org/zap"
 )
 
 func Start() {
@@ -22,8 +24,41 @@ func Start() {
 		fmt.Println("init logger error: ", err)
 	}
 
-	DBstore := postgres.New()
-	serv := service.New()
-	handler := handler.New()
-	router := router.New()
+	dbConn, err := postgres.Connect(cfg.DBURI)
+	if err != nil {
+		log.Error("db connection error", zap.Error(err))
+	}
+	defer dbConn.Close()
+
+	dbStore := postgres.New(dbConn)
+	serv := service.New(service.Storage{
+		Card:     &dbStore.Card,
+		User:     &dbStore.User,
+		Binary:   &dbStore.Binary,
+		LogoPass: &dbStore.LogoPass,
+	}, *cfg, log)
+
+	handler := handler.New(handler.Service{
+		Card:     &serv.Card,
+		User:     &serv.User,
+		Binary:   &serv.Binary,
+		LogoPass: &serv.LogoPass,
+	}, log)
+
+	router := router.New(router.Handler{
+		Card:     handler.Card,
+		User:     handler.User,
+		Binary:   handler.Binary,
+		LogoPass: handler.LogoPass,
+	})
+
+	srv := &http.Server{
+		Addr:    cfg.ServerAddress,
+		Handler: router,
+	}
+
+	log.Sugar().Infof("Starting server on addr: %v", srv.Addr)
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal("start web server error", zap.Error(err))
+	}
 }
