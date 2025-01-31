738d3f8a05d1e8f1e16b6e956f497e7c0140f44c
Zrossiz <soon.pluxury@mail.ru>
2025-01-31 13:54:22 +0300

    feat: added user notes

--- migrations/1_init.sql
+++ migrations/1_init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(20) UNIQUE NOT NULL,
-    password VARCHAR(60) NOT NULL,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS binary_data (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    title VARCHAR(100) NOT NULL
+    title TEXT NOT NULL
     binary_data BYTEA NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
@@ -18,9 +18,9 @@ CREATE TABLE IF NOT EXISTS binary_data (
 CREATE TABLE IF NOT EXISTS passwords (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    app_name VARCHAR(100) NOT NULL, 
-    username VARCHAR(200) NOT NULL,
-    password VARCHAR(100) NOT NULL,
+    app_name TEXT NOT NULL, 
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
@@ -28,10 +28,20 @@ CREATE TABLE IF NOT EXISTS passwords (
 CREATE TABLE IF NOT EXISTS cards (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    bankName VARCHAR(100),
-    num VARCHAR(20) NOT NULL,
-    cvv VARCHAR(4) NOT NULL,
-    exp_date VARCHAR(10) NOT NULL,
+    bank_name TEXT,
+    num TEXT NOT NULL,
+    cvv TEXT NOT NULL,
+    exp_date TEXT NOT NULL,
+    card_holder_name TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS notes (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    title TEXT NOT NULL,
+    text_data TEXT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
